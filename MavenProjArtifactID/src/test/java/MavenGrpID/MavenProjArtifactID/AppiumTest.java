59d1e89903a57b739ec06b99692a026ecdfab032
Ankitathoughts <ankanu9703@gmail.com>
2022-03-21 10:56:26 +0530

    Modified of First Commit

--- MavenProjArtifactID/src/test/java/MavenGrpID/MavenProjArtifactID/AppiumTest.java
+++ MavenProjArtifactID/src/test/java/MavenGrpID/MavenProjArtifactID/AppiumTest.java
@@ -10,6 +10,9 @@ public class AppiumTest {
 	public void appiumTestmethod()
 	{
 		System.out.println("appiumTestmethod_1");
+		System.out.println("appiumTestmethod_2");
+		System.out.println("appiumTestmethod_3");
+		
 	}
 
 	//Now run in cmd from the project directory as
